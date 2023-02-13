57d076b05f89fcbaea236e9389d39a01aff09f24
Kristina Degtyareva <bessonovac@yandex.ru>
2023-02-13 23:28:10 +0500

    Второе ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,176 +1,114 @@
-let popup = document.querySelector('.popup'); //находим popup в DOM
-let photoGrid = document.querySelector('.photo-grid__list');
-let popupClose = popup.querySelector('.popup__btn-close'); //находим кнопку закрытия в DOM
-let nameInput = document.getElementById('userName');// Находим поля формы в DOM
-let jobInput = document.getElementById('userJob');// Находим поля формы в DOM
-const formEdit = document.getElementById('formEdit');
-const formAdd = document.getElementById('formAdd');
-let formTitle = document.querySelector('.popup__title');
-let formEditOpen = document.getElementById('btn-edit');//находим кнопку редактирования в DOM
-let formAddOpen = document.getElementById('btn-add');
-let profile = document.querySelector('.profile'); //находим профиль в DOM
-let profileName = profile.querySelector('.profile__info-name');//находим поле имя пользователя в DOM
-let profileJob = profile.querySelector('.profile__info-job');//находим поле  пользователя в DOM
+const popupEditProfile = document.getElementById('popup-edit-profile'); //находим popup редактирования профиля в DOM
+const popupAddPhoto = document.getElementById('popup-add-photo');//находим popup добавления фото в DOM
+const popupViewPhoto = document.getElementById('popup-view-photo');//находим popup просмотра фото в DOM
+const photoGrid = document.querySelector('.photo-grid__list');
+const buttonEditClose = document.getElementById('close-edit-profile'); //находим кнопку закрытия в DOM
+const buttonAddClose = document.getElementById('close-add-photo');
+const formEdit = document.getElementById('formEdit');//находим форму редактирвоания профиляonst nameInput = document.getElementById('userName');// Находим поля формы в DOM
+const nameInput = document.getElementById('userName');// Находим поля формы в DOM
+const jobInput = document.getElementById('userJob');// Находим поля формы в DOM
+const formAdd = document.getElementById('formAdd'); //находим форму добавления фото
+const photoNameInput = document.getElementById('photoName');//находим инпуты
+const photoLinkInput = document.getElementById('photoLink');
+const formTitle = document.querySelector('.popup__title');
+const formEditOpen = document.getElementById('btn-edit');//находим кнопку редактирования в DOM
+const formAddOpen = document.getElementById('btn-add');
+const profile = document.querySelector('.profile'); //находим профиль в DOM
+const profileName = profile.querySelector('.profile__info-name');//находим поле имя пользователя в DOM
+const profileJob = profile.querySelector('.profile__info-job');//находим поле  пользователя в DOM
 const popupOpen = document.querySelectorAll('.btn-popup'); //находим кнопки редактирования и добавления и записываем в перемнную
-let photoNameInput = document.getElementById('photoName');
-let photoLinkInput = document.getElementById('photoLink');
-const Template = document.querySelector('#userPhoto').content;
-const photoElement = Template.querySelector('.photo-card').cloneNode(true);
-const buttonLike = Template.querySelectorAll('.photo-card__btn-like');
-//const resetButton = document.getElementsByClassName('photo-card__delete');
-//const popupPhoto = document.getElementsByClassName('popup-photo');
-const photoLink = document.querySelector('.popup-photo__container-view');
-const photoTitle = document.querySelector('.popup-photo__container-title');
-//console.log(popupPhoto, photoLink, photoTitle);
+const template = document.querySelector('#userPhoto').content;
+const buttonLike = template.querySelectorAll('.photo-card__btn-like');
+const photoLink = document.querySelector('.popup__container-view');
+const photoTitle = document.querySelector('.popup__container-title');
 const popupPhoto = document.querySelector('.popup-photo');
-const photoClose = document.querySelector('.popup-photo__container-close');
-photoPlace = [
-  {
-    name: 'Зеленоград, Россия',
-    link: 'https://images.unsplash.com/photo-1536577722576-fcfdbcad17e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80'
-  },
-  {
-    name: 'Республика Коми',
-    link: 'https://images.unsplash.com/photo-1525302220185-c387a117886e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'
-  },
-  {
-    name: 'Станция Пролетарская, г. Санкт-Петербург',
-    link: 'https://images.unsplash.com/photo-1588698947572-5563eed6d86a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
-  },
-  {
-    name: 'Воронеж',
-    link: 'https://images.unsplash.com/photo-1616398201291-56a3fa55457a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+const photoClose = document.querySelector('.popup__container-close');
 
-function viewPhoto() {
 
-}
 
-photoPlace.forEach(function (index) {
-  const photoElement = Template.querySelector('.photo-card').cloneNode(true);
-  photoElement.querySelector('.photo-card__image').src = index.link;
-  photoElement.querySelector('.photo-card__title').textContent = index.name;
-  photoElement.querySelector('.photo-card__image').alt = index.name;
-  const buttonLike = photoElement.querySelector('.photo-card__btn-like');
-  buttonLike.addEventListener('click', function (index) {
-  const indexTarget = index.target;
-  indexTarget.classList.toggle('photo-card__btn-like_active');
-  })
+// функция добавления карточек
+const createCard = (cardAdd) => {
+  const photoElement = template.querySelector('.photo-card').cloneNode(true);
+  photoElement.querySelector('.photo-card__image').src = cardAdd.link;
+  photoElement.querySelector('.photo-card__title').textContent = cardAdd.name;
+  photoElement.querySelector('.photo-card__image').alt = cardAdd.name;
 
-  photoElement.querySelector('.photo-card__image').addEventListener('click', function () {
-    popupPhoto.classList.add('popup-photo_opened');
-    photoLink.src = photoElement.querySelector('.photo-card__image').src;
-    photoTitle.textContent = photoElement.textContent;
-  })
-
-  photoClose.addEventListener('click', function () {
-    popupPhoto.classList.remove('popup-photo_opened');
-  })
-
-  const resetButton = photoElement.querySelector('.photo-card__delete');
-  resetButton.addEventListener('click', () => {
-    photoElement.remove();
-  })
-
-  photoGrid.append(photoElement);
-
-});
-
-function PhotoAdd(e) {
-  console.log('Submit сработал');
-  e.preventDefault();
-  const photoElement = Template.querySelector('.photo-card').cloneNode(true);
-  photoElement.querySelector('.photo-card__image').src = photoLinkInput.value;
-  photoElement.querySelector('.photo-card__title').textContent = photoNameInput.value;
-  photoElement.querySelector('.photo-card__image').alt = photoNameInput.value;
-  const buttonLike = photoElement.querySelector('.photo-card__btn-like');
+  //функция обработки кнопки 
+  const buttonLike = photoElement.querySelector('.photo-card__btn-like'); лайка
   buttonLike.addEventListener('click', function (index) {
     const indexTarget = index.target;
     indexTarget.classList.toggle('photo-card__btn-like_active');
   })
 
+  //функция обработки кнопки удаления карточки
   const resetButton = photoElement.querySelector('.photo-card__delete');
-  console.log(resetButton);
   resetButton.addEventListener('click', () => {
     photoElement.remove();
   })
 
-  photoElement.querySelector('.photo-card__image').addEventListener('click', function () {
-    popupPhoto.classList.add('popup-photo_opened');
+  //открытие попапа просмотра фотографии
+  photoElement.querySelector('.photo-card__image').addEventListener('click', () => {
+    openPopup(popupViewPhoto);
     photoLink.src = photoElement.querySelector('.photo-card__image').src;
     photoTitle.textContent = photoElement.textContent;
   })
 
-  photoClose.addEventListener('click', function () {
-    popupPhoto.classList.remove('popup-photo_opened');
-  })
-
-  photoGrid.prepend(photoElement);
-  closePopupForm();
-  photoLinkInput.value = '';
-  photoNameInput.value = '';
+//возвращаем картоку
+  return photoElement;
 }
 
-//функция открытия формы
-popupOpen.forEach((index) => { //перебираем NodeList, отслеживая какая кнопка была нажата
-  index.addEventListener('click', () => {
-    popup.classList.add('popup_opened'); //открываем popup
-    if (index.classList.contains('profile__btn-add')) {//проверяем условие, если у кнопки имеется класс profile__btn-add, то
-      formAdd.classList.add('form-edit_opened');//открываем форму добавление фотографий
-      formTitle.textContent = 'Новое место';//меняем заголовок формы
-      formEdit.classList.remove('form-edit_opened');
-    }
-    if (index.classList.contains('profile__btn-edit')) {//проверяем условие, если у кнопки имеется класс profile__btn-edit, то
-      formEdit.classList.add('form-edit_opened');//открываем форму редактирования профиля
-      nameInput.value = profileName.textContent; //заносим данные в форму
-      jobInput.value = profileJob.textContent;
-      formTitle.textContent = 'Редактировать профиль';//меняем заголовой формы
-      formAdd.classList.remove('form-edit_opened');
+//вставляем карточку на страницу
+const renderCard = (cardAdd) => {
+  photoGrid.prepend(createCard(cardAdd));
+}
 
-    }
-  })
+//загружаем карточки из массива
+photoPlace.forEach((item) => {
+  renderCard(item);
 })
 
-function closePopupForm() { //функция закрытия popup
-  popup.classList.remove('popup_opened');//удаляем класс у popup, закрывая его
-  //удаляем класс у формы, закрывая форму
+//загружаем карточки из формы закгрузки
+const photoAddFromForm = (evt) => {
+  evt.preventDefault();
+  const newCard = {
+    link: photoLinkInput.value,
+    name: photoNameInput.value
+  }
+
+  renderCard(newCard);
+
+  photoLinkInput.value = '';
+  photoNameInput.value = '';
+  closePopup(popupAddPhoto);
 
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+//функция открытия попапов
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent; //заносим данные в форму
+  jobInput.value = profileJob.textContent;
+}
+
+
+//функция закрытия попапов
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+//функция обработки кнопки отправки формы
 function handleFormSubmit(e) {
-  console.log('Submit сработал');
   e.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   profileName.textContent = nameInput.value; // Получите значение полей jobInput и nameInput из свойства value
   profileJob.textContent = jobInput.value;// Получите значение полей jobInput и nameInput из свойства value
-  closePopupForm();//вызов функции закрытия popup
-}
-
-function photoFormSubmit(e) {
-  e.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  closePopupForm();//вызов функции закрытия popup
+  closePopup(popupEditProfile);//вызов функции закрытия popup
 }
 
 
-popupClose.addEventListener('click', closePopupForm);//обработчик события закрытия popup
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formEdit.addEventListener('submit', handleFormSubmit);
-formAdd.addEventListener('submit', PhotoAdd);
-
-// buttonLike.forEach((index) => {
-//   index.addEventListener("click", () => {
-//     console.log(index);
-//     index.classList.toggle('photo-card__btn-like_active');
-//   })
-// });
+formEditOpen.addEventListener('click', () => openPopup(popupEditProfile));//обработчик события открытия popup редактиварония профиля
+formAddOpen.addEventListener('click', () => openPopup(popupAddPhoto));//обработчик события открытия popup добавления карточки
+buttonAddClose.addEventListener('click', () => closePopup(popupAddPhoto));///обработчик события закрытия  зopup добавления карточки
+buttonEditClose.addEventListener('click', () => closePopup(popupEditProfile));//обработчик события закрытия popup редактиварония профиля
+photoClose.addEventListener('click', () => closePopup(popupViewPhoto));//обработчик события закрытия popup просмотра фотографии
+formEdit.addEventListener('submit', handleFormSubmit); //обработка кнопки формы редактирования профиля
+formAdd.addEventListener('submit', photoAddFromForm);//обработка кнопки формы загрузки карточки
